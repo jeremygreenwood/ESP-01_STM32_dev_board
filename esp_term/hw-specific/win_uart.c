9006056fd0e6ca13a7f96eda121d20c59677c5e8
Eric Byers <eric.flyn.b@gmail.com>
2015-10-07 23:01:11 -0700

    Implemented Windows UART interface.

--- esp_term/hw-specific/win_uart.c
+++ esp_term/hw-specific/win_uart.c
@@ -2,19 +2,13 @@
     Includes
 **************************************************/
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <windows.h>
 #include "uart.h"
-
-
-/* NB:  This file is mostly just a stub.  Never been used.  */
 
 /**************************************************
     Global
 **************************************************/
-HANDLE fd;
+static HANDLE fd;
 
 /**************************************************
     uart_open
@@ -25,18 +19,44 @@ int uart_open
     int flags
     )
 {
-fd = CreateFile( path,  
+DCB parameters;
+
+fd = CreateFile( path, // example "\\\\.\\COM4"
                  GENERIC_READ | GENERIC_WRITE, 
                  0, 
-                 0, 
+                 NULL,
                  OPEN_EXISTING,
-                 FILE_FLAG_OVERLAPPED,
-                 0);
-if (fd == INVALID_HANDLE_VALUE)
+				 FILE_ATTRIBUTE_NORMAL,
+                 NULL );
+
+if( fd == INVALID_HANDLE_VALUE )
     {
-    printf("Problem opening port\n");
-    return;
+    printf( "Problem opening port.\n" );
+    return( 1 );
     }
+
+parameters.DCBlength = sizeof( parameters );
+
+if( GetCommState( fd, &parameters ) == 0 )
+{
+    printf( "Problem getting parameters.\n");
+    CloseHandle( fd );
+    return( 1 );
+}
+
+parameters.BaudRate = CBR_115200;
+parameters.ByteSize = 8;
+parameters.StopBits = ONESTOPBIT;
+parameters.Parity   = NOPARITY;
+
+if(SetCommState(fd, &parameters) == 0)
+{
+    printf( "Problem setting parameters.\n" );
+    CloseHandle( fd );
+    return( 1 );
+}
+
+return( 0 );
 }
 
 /**************************************************
@@ -47,8 +67,7 @@ int uart_close
     void
     )
 {
-printf("uart_close\n");
-return( 0 );
+return( CloseHandle( fd ) );
 }
 
 /**************************************************
@@ -60,8 +79,16 @@ int uart_write
     int size
     )
 {
-printf("uart_write\n");
-return( 0 );
+DWORD bytes_written;
+
+if( !WriteFile( fd, buf, size, &bytes_written, NULL ) )
+{
+	printf( "Problem with UART write.\n" );
+	CloseHandle( fd );
+	return( 1 );
+}
+
+return( bytes_written );
 }
 
 /**************************************************
@@ -74,117 +101,14 @@ int uart_read
     )
 {
 DWORD read_num;
-int total;
 
-ReadFile(fd, buf, size, &read_num, NULL);
-      // if (GetLastError() != ERROR_IO_PENDING)
-total = (int)read_num;
-
-if(total > 0)
-    {
-    printf("uart_read[%d]: ---%s---\n", total, buf);
-    }
+if( !ReadFile( fd, buf, size, &read_num, NULL ) )
+{
+	printf( "Problem with UART read.\n" );
+	CloseHandle( fd );
+	return( 1 );
+}
 
 return( (int)read_num );
 }
-
-
-
-
-
-
-
-
-#ifdef SOME_RAND_CODE
-
-// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-E_boolean OpenCom1(void)
-{
-   COMMTIMEOUTS timeouts;
-
-   comPorthandle = CreateFile("COM1",  // Specify port device: default "COM1"
-   GENERIC_READ | GENERIC_WRITE,       // Specify mode that open device.
-   0,                                  // the device isn't shared.
-   NULL,                               // the object gets a default security.
-   OPEN_EXISTING,                      // Specify which action to take on file.
-   0,                                  // default (not overlapped i/o).
-   NULL);                              // default (hTemplate must be NULL for COM devices).
-
-   if (comPorthandle == INVALID_HANDLE_VALUE)
-      return False;
-
-   deviceControlBlock.DCBlength = sizeof(deviceControlBlock);
-
-    if((GetCommState(comPorthandle, &deviceControlBlock) == 0))
-    {
-      // CodeMe: do what?
-      return False;
-    }
-
-    deviceControlBlock.BaudRate = CBR_115200;
-    deviceControlBlock.StopBits = ONESTOPBIT;
-    deviceControlBlock.Parity   = NOPARITY;
-    deviceControlBlock.ByteSize = DATABITS_8;
-    deviceControlBlock.fRtsControl = 0;
-
-    if (!SetCommState(comPorthandle, &deviceControlBlock))
-    {
-      // CodeMe: do what?
-      return False;
-    }
-
-#if 0
-// alternative to GetCommState() and SetCommState()
-// both versions succeed
-   if (!BuildCommDCB("115200,n,8,1", &deviceControlBlock))
-    {
-      // CodeMe: do what?
-      return False;
-    }
-#endif 
-
-    // set short timeouts on the comm port.
-    timeouts.ReadIntervalTimeout = 0;
-    timeouts.ReadTotalTimeoutMultiplier = 1;
-    timeouts.ReadTotalTimeoutConstant = 1;
-    timeouts.WriteTotalTimeoutMultiplier = 1;
-    timeouts.WriteTotalTimeoutConstant = 1;
-    if (!SetCommTimeouts(comPorthandle, &timeouts))
-    {
-      // CodeMe: do what?
-      return False;
-    }
-
-   return True;
-}//OpenCom1()
-
-// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-void      ReadCharacterFromCom1(INPUT char *theCharacter)
-{
-   DWORD numBytesRead;
-
-   numBytesRead = 0;
-
-   while (numBytesRead == 0)
-   {
-      ReadFile(comPorthandle,           // handle of file to read
-               theCharacter,            // store read data here
-               sizeof(theCharacter),    // number of bytes to read
-               &numBytesRead,           // pointer to number of bytes actually read
-               NULL);
-   }
-   return;
-}//ReadCharacterFromCom1()
-
-
-#endif
-
-
-
-
-
-
-
-
-
 
