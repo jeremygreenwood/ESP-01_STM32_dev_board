0ffe53b535961d6be4a7c0cdfd952efee5959577
kangasp <philip.kangas@gmail.com>
2015-11-11 23:58:31 -0800

    Adjusting the callback to use templates.  Currently broken.

--- esp_term/prj/callbacks/at_cb.c
+++ esp_term/prj/callbacks/at_cb.c
@@ -2,6 +2,8 @@
 #include "dbg_log.h"
 #include "uart.h"
 #include "cb.h"
+#include "templates.h"
+
 
 /**************************************************
 
@@ -15,11 +17,15 @@ push it all into esp_server.
 
 Leaving here for now, because compiling is nice. 
 **************************************************/
-char resp_http_ok[] = 
+char resp_http_ok_long[] = 
 "HTTP/1.1 200 OK\r\n"                                 \
 "Date: Fri, 11 Fri 2015 06:25:59 GMT\r\n"             \
 "Content-Type: text/html\r\n"                         \
 "Content-Length: %u\r\n\r\n%s";
+
+char resp_http_ok[] = 
+"HTTP/1.1 200 OK\r\n"                               \
+"Content-Length: %d\r\n\r\n";
 
 char resp_http_body[] = 
 "<html><body>"                                      \
@@ -28,6 +34,9 @@ char resp_http_body[] =
 "<h3>Here's all the raw data:\n</h3>"               \
 "<h5>%s\n</h5>"             \
 "</body></html>\n\n";
+
+void send_html(int id, unsigned char *data, int data_len );
+
 
 /**************************************************
     cb_ipd
@@ -42,10 +51,10 @@ Local Variables
 ---------------------------------- */
 at_cb_request_type      at_cb_req;
 char                    cmd_buf[512];
-char                    hdr_buf[1024];
-char                    body_buf[1024];
+//char                    hdr_buf[1024];
+//char                    body_buf[1024];
 static int              id;
-int                     cont_len;
+//int                     cont_len;
 int                     i;
 
 /*---------------------------------- 
@@ -73,17 +82,30 @@ switch( ret->cmd )
         id = strtol( &ret->raw[5], NULL, 10);
         dbg_log( "Sending HTML to %d\n", id );
 
-        sprintf(body_buf, resp_http_body, id, ret->raw);
-        sprintf(hdr_buf, resp_http_ok, strlen(body_buf), body_buf);
-        cont_len = strlen(hdr_buf);
-        sprintf(cmd_buf, "=%d,%d", id, cont_len);
+        if( strncmp( &ret->raw[11], "GET / HTTP", 6 ) == 0 )
+            {
+            send_html( id, index_html, index_html_len );
+            }
+        else if( strncmp( &ret->raw[11], "GET /bootstrap.css", sizeof("GET /bootstrap.css") ) == 0 )
+            {
+            send_html( id, bootstrap_css, bootstrap_css_len );
+            }
+        else if( strncmp( &ret->raw[11], "GET /starter-template.css", sizeof("GET /starter-template.css") ) == 0 )
+            {
+            send_html( id, starter_template_css, starter_template_css_len );
+            }
+
+        //sprintf(body_buf, resp_http_body, id, ret->raw);
+        //sprintf(hdr_buf, resp_http_ok, strlen(body_buf), body_buf);
+        //cont_len = strlen(hdr_buf);
+        //sprintf(cmd_buf, "=%d,%d", id, cont_len);
         
         // dbg_log( "cmd_buf[%u]: %s\n\n", strlen(cmd_buf), cmd_buf );
         // dbg_log( "hdr_buf[%u]: %s\n\n", cont_len, hdr_buf );
        
-        at_send_cmd_with_text(AT_CMD_CIPSEND, cmd_buf, strlen(cmd_buf));
-        sleep(1);
-        uart_write(hdr_buf, strlen(hdr_buf));
+        //at_send_cmd_with_text(AT_CMD_CIPSEND, cmd_buf, strlen(cmd_buf));
+        //sleep(1);
+        //uart_write(hdr_buf, strlen(hdr_buf));
         
         /*
 This uart write needs to be moved into the next cb.
@@ -210,4 +232,41 @@ switch( ret->cmd )
     
 return(0);
 }
+
+
+
+/**************************************************
+    send_html
+**************************************************/
+void send_html
+    ( 
+    int                 id,
+    unsigned char      *data,
+    int                 data_len
+    )
+{
+/*---------------------------------- 
+local variables
+---------------------------------- */
+char cmd_buf[64];
+char hdr_buf[128];
+int  full_len;
+
+/*---------------------------------- 
+local variables
+---------------------------------- */
+sprintf(hdr_buf, resp_http_ok, data_len);
+full_len = strlen(hdr_buf) + data_len;
+sprintf(cmd_buf, "=%d,%d", id, full_len);
+        
+dbg_log( "cmd_buf[%u]: %s\n\n", strlen(cmd_buf), cmd_buf );
+// dbg_log( "hdr_buf[%u]: %s\n\n", cont_len, hdr_buf );
+       
+at_send_cmd_with_text(AT_CMD_CIPSEND, cmd_buf, strlen(cmd_buf));
+sleep(1);
+uart_write(hdr_buf, strlen(hdr_buf));
+uart_write((char*)data, data_len);
+
+}
+
 
